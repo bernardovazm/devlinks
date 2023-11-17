4f245190f2edd4c5c7be12a7edc66a5d7785db03
Bernardo Vaz <bernardovazmelo@gmail.com>
2023-11-17 17:01:37 -0300

    feat: add favicon emoji function

--- script.js
+++ script.js
@@ -1,8 +1,80 @@
 /* 
-
     instructions and syntaxes
-
 */
+
+const iconsList = [
+	"📷",
+	"📄",
+	"🐱‍💻",
+	"🐱‍👤",
+	"🐱‍🚀",
+	"🐱‍👓",
+	"🐱‍🐉",
+	"🔋",
+	"🔗",
+	"🔧",
+	"🔨",
+	"🎉",
+	"🎲",
+	"🚀",
+	"👨‍🚀",
+	"🧱",
+	"🔬",
+	"🧰",
+	"🎬",
+	"📺",
+	"📚",
+	"📁",
+	"💡",
+	"🧪",
+	"⚡",
+	"🎯",
+	"♟️",
+	"🎨",
+];
+
+function randomIntNumber(range = 2) {
+	return Math.floor(Math.random() * range);
+}
+
+function setFavicon(icon = "📄") {
+	const favicon = document.querySelector("link[rel~='icon']");
+
+	favicon.href =
+		"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 110 100%22><text y=%22.9em%22 font-size=%2290%22>" +
+		icon +
+		"</text></svg>";
+}
+
+function setRandomIcon() {
+	setFavicon(iconsList[randomIntNumber(iconsList.length)]);
+}
+
+function setFaviconDevice() {
+	const iconsDevice = { desktop: "🖥️", laptop: "💻", mobile: "📱" };
+
+	if (window.innerWidth <= 800) {
+		setFavicon(iconsDevice.mobile);
+	} else if (window.innerWidth <= 1400) {
+		setFavicon(iconsDevice.laptop);
+	} else {
+		setFavicon(iconsDevice.desktop);
+	}
+}
+
+setFavicon("⚙️");
+setTimeout(() => {
+	setRandomIcon();
+}, 400);
+setTimeout(() => {
+	setRandomIcon();
+}, 800);
+setTimeout(() => {
+	setRandomIcon();
+}, 1500);
+setTimeout(() => {
+	setFaviconDevice();
+}, 3000);
 
 function toggleMode() {
 	const html = document.documentElement;
